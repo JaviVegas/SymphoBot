26dec199a93a2eb793027f990ac66e38fe0402a8
JaviVegas <mjavivegas2@gmail.com>
2024-09-13 15:28:02 -0300

    Minor optimizations.

--- src/music-manager.py
+++ src/music-manager.py
@@ -13,27 +13,30 @@ def extract_info_from_description(description):
        
     
 #solo descargo el audio
-def download_song(url,key, TEMP_PATH):
+def download_video(url, key, dicci, temp_path):
     '''
-    downloads the requested song
+    Downloads the requested video as an audio file.
 
     Parameters
     ----------
-    url : String
-        The video/playlist url
+    url : str
+        The video's URL
     key: String
-        The song identifier
-    TEMP_PATH: String
-        Absolute path to the 'temp' directory
+        The video's identifier, obtained from the URL.
+    dicci: dict
+        A dictionary that represents all previously downloaded files.
+    TEMP_PATH: str
+        Absolute path to the 'temp' directory, where the file will be saved.
+
     Returns
     -------
-    string
-        
+    dict
+        An updated version of the dictionary that represents all downloaded files, including the latest file.
     '''    
     #ffmpeg\fmpeg.exe
     absolute_exe_path= convert_to_absolute("ffmpeg/ffmpeg.exe")
     name='%(title)s.%(ext)s'; 
-    path_name=convert_to_absolute(TEMP_PATH+"/"+name)
+    path_name=convert_to_absolute(temp_path + "/" + name)
     
     ydl_opts = {
         'format': 'bestaudio/best',  
@@ -47,16 +50,17 @@ def download_song(url,key, TEMP_PATH):
         'outtmpl': path_name,   
     }
    
-    dicci={}
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
         title = info.get('title', 'Desconocido')
         uploader = info.get('uploader', 'Desconocido')
         
       
-        dicci[key]={'Title':title,
+        dicci[key]={'Title': title,
                     'Artist': uploader,
-                    'Relative-Path':convert_to_relative(path_name)}
+                    'Relative-Path': convert_to_relative(path_name)
+                    }
+    
     return dicci
 #[{url:{titulo: blala, 
 #     artista: blabla
@@ -64,48 +68,52 @@ def download_song(url,key, TEMP_PATH):
 #{}
 #]
 
-def cut_url(url,inichar,endchar): 
+def cut_url(url, inichar, endchar): 
     '''
-    obtains the url identifier
+    Obtains an identifer for the video from its URL.
 
     Parameters
     ----------
-    url : String
-        The video/playlist url
+    url: str
+        The video's URL.
     inichar: char
-        initial identifier delimiter
+        Identifier's initial delimiter.
     endchar: char 
-        end delimiter of the identifier
+        Identifier's end delimiter.
+
     Returns
     -------
-    string
-        
+    str
+         The video's identifier, obtained from the URL.
     '''    
     initialpos = url.find(inichar)
     print(initialpos)
     endpos=url.find(endchar)
     print(initialpos)
     if endpos != -1: 
-        key= url[(initialpos+1): (endpos -1)]
+        key= url[(initialpos+1) : (endpos -1)]
     else: 
-        key= url[(initialpos+1): (len(url))]  
+        key= url[(initialpos+1) : (len(url))]  
     return key
             
             
             
 def process_url(url):
     '''
-    process url
+    Processes the video's URL, based on its format, in order to obtain an identifier from it.
 
     Parameters
     ----------
-    url : String
-        The video/playlist url
+    url: str
+        The video's URL.
         
     Returns
     -------
-    string/None
-        
+    str
+        The video's identifier, obtained from the URL.
+    or
+    None
+        Represents the video was not found.
     '''
     if url.starswith("https://www.youtube.com/watch?v=") or url.startswith("https://m.youtube.com/watch?v=") : 
         key=cut_url(url, '=', '&')
@@ -115,49 +123,53 @@ def process_url(url):
         return key
     else: 
         return None
-
-
-
+    
 
 def get_song(url):
     '''
-    tries to open the json file and checks for the existence of the song. 
-    If it does not exist, it downloads it and saves its data. 
+    Checks if the video file was already downloaded. 
+    If it wasn't, then it gets downloaded and its information saved in a json file.
 
     Parameters
     ----------
-    url : String
-        The video/playlist url
+    url : str
+        The video's URL.
         
     Returns
     -------
-    string
-        
+    str
+        The video's identifier, obtained from the URL.
+    or
+    None
+        Represents the video was not found.
     '''
-    TEMP_PATH= convert_to_absolute('temp')
+    temp_path= convert_to_absolute('temp')
     key= process_url
     json_path=convert_to_absolute('data/history.json')
     try: 
-        read_json(json_path)
-        if (key is not None) and (key not in dicci): 
-  
-            dicci=download_song(url, key,TEMP_PATH)
-            write_json(json_path, dicci)
+        dicci = read_json(json_path)
+        if (key is not None) and (key not in dicci):  
+            new_dicci=download_video(url, key, dicci, temp_path)
+            write_json(json_path, new_dicci)
+
         elif key is not None:
-            #busco la cancion en dicci y la reproduczo PARA IMPLEMENTAR
+            # busco la cancion en dicci y la reproduczo
+            # PARA IMPLEMENTAR
             pass
+
     except FileNotFoundError: 
         if key is not None: 
-            dicci=download_song(url, key,TEMP_PATH)
+            dicci=download_video(url, key, temp_path)
             write_json(json_path, dicci)
+    
     return key
          
         
         
         
 
-video_web_url = "https://www.youtube.com/watch?v=efS8lO4nCtQ&ab_channel=juanjannon4001"
-video_app_url="https://youtu.be/efS8lO4nCtQ?si=kBotFy7AOteH1A49"
+# video_web_url = "https://www.youtube.com/watch?v=efS8lO4nCtQ&ab_channel=juanjannon4001"
+# video_app_url="https://youtu.be/efS8lO4nCtQ?si=kBotFy7AOteH1A49"
 
-get_song(video_web_url)
-print ("todo ok")
+# get_song(video_web_url)
+# print ("todo ok")
