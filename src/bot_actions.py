f12a90dbc386763ef58bd4a0707105a453bff257
bianca defelipe <biancadef03@gmail.com>
2024-09-15 09:46:12 -0300

    correcting the implementation of the play command

--- src/bot_actions.py
+++ src/bot_actions.py
@@ -2,16 +2,24 @@ import asyncio
 from discord import Client, Message, User, FFmpegPCMAudio
 from src.music_manager import get_song
 
-async def join(user: User):
-    voice_client = await user.voice.channel.connect()
-    return voice_client    
+#async def join(user: User):
+  #  voice_client = await user.voice.channel.connect() <- anterior
+  #  return voice_client    
+async def join(message: Message): 
+    if message.author.voice and message.author.voice.channel:
+        voice_channel = message.author.voice.channel
+        voice_client = await voice_channel.connect()
+        return voice_client
+    else:
+        print ( "UUSUARIO NO ESTA EN EL CANAL DE VOZ")
+        raise Exception("El usuario no está en un canal de voz")
 
-def play(url: str):
+async def play(url: str):
     #loop = asyncio.get_event_loop()
     
     #aca necesitamos el info
     player = None
-    song = get_song(url)
+    song =  await get_song(url)
     ffmpeg_options = {"options": "-vn"}
     if (song is not None):
         player = FFmpegPCMAudio(song['url'], **ffmpeg_options)
